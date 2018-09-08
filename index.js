9393c2553a561c56b43691b679c125306957e20a
i5irin <contact@i5ir.in>
2018-09-09 00:47:01 +0900

    Push通知登録、解除のAPI追加

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const RABBITMQ_URL = process.env.RABBITMQ_URL || 'apqp://guest:guest@localhost:5
 
 const app = express();
 const open = amqplib.connect(RABBITMQ_URL);
+// TODO: 本当はDBにでもしまってpushサーバー側で宛先に対応するの取り出すべき
+// push設定の登録自体はアカウント単位でもっておく
+// 今はメッセージ内容と一緒にメッセージブローカーへ投げてるｗ
+// Push通知先情報
+// endpoint: Push通知リクエスト先, authSecret: クライアントシークレット
+// contentEncoding: 通知内容エンコード方式, publicKey: クライアント公開鍵
+const pushRecipientConfigs = [];
 
 app.use(express.json());
 
@@ -29,6 +36,26 @@ app.post('/messages', async (req, res) => {
   channel.sendToQueue('wave-messages', new Buffer(JSON.stringify(req.body)));
   console.log(req.body);
   res.send('ok');
+});
+
+
+// Push購読API
+app.post('/push/register', async (req, res) => {
+  // Push先を登録(識別とか一切していないおひとりさま用。大体がそうね)
+  const { endpoint, authSecret, contentEncoding, publicKey } = req.body;
+  pushRecipientConfigs.push({
+    endpoint, authSecret, contentEncoding, publicKey
+  });
+  console.log(endpoint, authSecret, contentEncoding, publicKey);
+  // FIXME:
+  res.status(201).json({ endpoint, authSecret, contentEncoding, publicKey });
+});
+
+// Push解約API
+app.delete('/push/unsubscribe', async (req, res) => {
+  const recipientInfo = pushRecipientConfigs.pop();
+  console.log(recipientInfo);
+  res.sendStatus(204);
 });
 
 app.listen(PORT, () => console.log(`listening to port ${PORT}`));
