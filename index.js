2ec213f9797abaee1bb8ad303c2e81072321cbac
i5irin <contact@i5ir.in>
2018-09-09 00:27:29 +0900

    CORS対応

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const open = amqplib.connect(RABBITMQ_URL);
 
 app.use(express.json());
 
+// CORS許可
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE, OPTIONS');
+  next();
+})
+
+// プリフライトリクエスト用
+app.options('*', (req, res) => {
+  res.sendStatus(200);
+});
+
 app.post('/messages', async (req, res) => {
   const connection = await open;
   const channel = await connection.createChannel();
